a71dd4f6cdcca2f8d9073e57db44ff9899cf46e2
Cash Myers <cmyers880@gmail.com>
2025-06-02 18:56:58 -0500

    feat: Added full CRUD functionality for Posts

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed 
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 from flask_login import current_user 
@@ -49,3 +49,7 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError('That email is already registered. Please choose a different one.')
             
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(min=2, max=100)])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
