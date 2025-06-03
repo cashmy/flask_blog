8f5cc5366025868d74441cdb560822e165050054
Cash Myers <cmyers880@gmail.com>
2025-06-03 16:21:56 -0500

    feat: Added customer error pages: 403, 404, 500

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -25,10 +25,12 @@ def create_app(config_class=Config):
     with app.app_context():
         from flaskblog.users.routes import users 
         from flaskblog.posts.routes import posts 
-        from flaskblog.main.routes import main  
+        from flaskblog.main.routes import main
+        from flaskblog.errors.handlers import errors  
 
         app.register_blueprint(users)
         app.register_blueprint(posts)
         app.register_blueprint(main)
+        app.register_blueprint(errors)
 
     return app
